d524881b21b919d94652461cf2ee918b84d2bcca
Ethan <ethan190c@gmail.com>
2019-07-08 22:52:20 -0400

    upgraded flutter version

---
+++ lib/screens/scanning/scan_screen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:camera/camera.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+
+import 'package:receipt_bubble/screens/scanning/confirm_scan_screen.dart';
+
+
+
+
+
+// A screen that allows users to take a picture using a given camera.
+class ScanScreen extends StatefulWidget {
+  // final CameraDescription camera;
+
+  const ScanScreen({
+    Key key,
+    // this.camera,
+  }) : super(key: key);
+
+  @override
+  ScanScreenState createState() => ScanScreenState();
+}
+
+class ScanScreenState extends State<ScanScreen> {
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+  List<CameraDescription> cameras;
+
+  bool isReady = false;
+  bool showCamera = true;
+  @override
+  void initState() {
+    super.initState();
+    setupCameras();
+
+    // To display the current output from the Camera,
+    // create a CameraController.
+    // _controller = CameraController(
+    //   // Get a specific camera from the list of available cameras.
+    //   widget.camera,
+    //   // Define the resolution to use.
+    //   ResolutionPreset.medium,
+    // );
+
+    // Next, initialize the controller. This returns a Future.
+    // _initializeControllerFuture = _controller.initialize();
+  }
+
+  Future<void> setupCameras() async {
+    try {
+      cameras = await availableCameras();
+      _controller = new CameraController(cameras[0], ResolutionPreset.medium);
+      await _controller.initialize();
+    } on CameraException catch (_) {
+      setState(() {
+        isReady = false;
+      });
+    }
+    setState(() {
+      isReady = true;
+    });
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar(title: Text('Take a picture')),
+      // Wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner
+      // until the controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.camera_alt),
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
+
+            // Construct the path where the image should be saved using the 
+            // pattern package.
+            final path = join(
+              // Store the picture in the temp directory.
+              // Find the temp directory using the `path_provider` plugin.
+              (await getTemporaryDirectory()).path,
+              '${DateTime.now()}.png',
+            );
+
+            // Attempt to take a picture and log where it's been saved.
+            await _controller.takePicture(path);
+
+            // If the picture was taken, display it on a new screen.
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(imagePath: path),
+              ),
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+      ),
+    );
+  }
+}
