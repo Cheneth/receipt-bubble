3bd29c8cf468465a2cf43ea8a4919d309d4b9cd9
Ethan <ethan190c@gmail.com>
2019-08-11 20:47:01 -0400

    cleaning up

--- lib/screens/scanning/confirm_scan_screen.dart
+++ lib/screens/scanning/confirm_scan_screen.dart
@@ -10,20 +10,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:random_string/random_string.dart';
 
 import 'package:receipt_bubble/models/RtReceiptModel.dart';
-// class DisplayPictureScreen extends StatelessWidget {
-//   final String imagePath;
-//   const DisplayPictureScreen({Key key, this.imagePath}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: Text('Display the Picture')),
-//       // The image is stored as a file on the device. Use the `Image.file`
-//       // constructor with the given path to display the image.
-//       body: Image.file(File(imagePath)),
-//     );
-//   }
-// }
 
 final formatCurrency = new NumberFormat.simpleCurrency();
 final FirebaseDatabase _database = FirebaseDatabase.instance;
